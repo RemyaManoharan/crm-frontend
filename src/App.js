df477be1fc7987230e9cdaf29054545b53a56630
RemyaManoharan <remyajinil@outlook.com>
2024-03-25 14:05:34 +0100

    search form added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Entry from "./page/entry/Entry.page";
 import Defaultlayout from "./layout/Defaultlayout";
 import DashboardPage from "./page/dashboard/DashboardPage";
 import AddTicketPage from "./page/NewTicket/AddTicketPage";
+import TicketListPage from "./page/TicketListing/TicketListPage";
 
 
 
@@ -13,7 +14,8 @@ function App() {
       {/* <Entry /> */}
       <Defaultlayout>
         {/* <DashboardPage/> */}
-      <AddTicketPage/>
+      {/* <AddTicketPage/> */}
+      <TicketListPage/>
         </Defaultlayout>
     </div>
   );
